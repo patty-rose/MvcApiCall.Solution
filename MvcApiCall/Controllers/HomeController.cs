87e4799bacca3e9d393e5136abb11bc4e821eec0
Patty Otero <101524326+patty-rose@users.noreply.github.com>
2022-08-15 20:37:49 -0700

    refactor to protect sensitive passwords

--- MvcApiCall/Controllers/HomeController.cs
+++ MvcApiCall/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ namespace MvcApiCall.Controllers
     {
         public IActionResult Index()
         {
-            var allArticles = Article.GetArticles("[YOUR-API-KEY-HERE]");//because GetArticles() is a static method, it's called on the Article class itself.
+            var allArticles = Article.GetArticles(EnvironmentVariables.ApiKey);//because GetArticles() is a static method, it's called on the Article class itself.
 
             return View(allArticles);
         }
---
+++ MvcApiCall/Models/EnvironmentalVariables.cs
@@ -0,0 +1,7 @@
+namespace MvcApiCall.Models
+{
+    public static class EnvironmentVariables
+    {
+        public static string ApiKey = "goJIpDLVGSytpsALpO3B0YsAwwAj0CLo";
+    }
+}
